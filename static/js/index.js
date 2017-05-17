315a4203f68a6a50ef9365aba8347d9931541315
John Pallag <john.pallag@webble.com>
2017-05-17 10:05:20 -0700

    Made default virus radius bigger

--- static/js/index.js
+++ static/js/index.js
@@ -183,7 +183,7 @@ $(document).ready(function() {
     signup($("#email").val(), $("#password").val(), {
       color: '#'+Math.floor(Math.random()*16777215).toString(16),
       image: getRandomInt(0, 6),
-      theshold: 0.1
+      theshold: 1
     });
     e.preventDefault();
   });
--- virus.js
+++ virus.js
@@ -2,7 +2,7 @@
 
 const Virus = function(id, params){
   this.id = id;
-  this.threshold = params.threshold || 0.1;
+  this.threshold = params.threshold || 1;
   this.params = params;
 };
 
